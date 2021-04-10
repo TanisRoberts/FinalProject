97603dd923be866af60011a70cddf25ba3b9d534
Tanis.Roberts <tanis.rc@googlemail.com>
2021-04-10 23:28:21 +0100

    Game Complete :)

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import arcade
+import neat
 from res.constants import *
 
 class PlayerSprite(arcade.Sprite):
@@ -9,13 +10,13 @@ class PlayerSprite(arcade.Sprite):
         
         self.scale = SPRITE_SCALING_PLAYER
         
-        self.idle_texture_pair = arcade.load_texture_pair(f"{SPRITE_FILE_PATH}_idle.png")
-        self.jump_texture_pair = arcade.load_texture_pair(f"{SPRITE_FILE_PATH}_jump.png")
-        self.fall_texture_pair = arcade.load_texture_pair(f"{SPRITE_FILE_PATH}_fall.png")
+        self.idle_texture_pair = arcade.load_texture_pair(f"{PLAYER_SPRITE_FILE_PATH}_idle.png")
+        self.jump_texture_pair = arcade.load_texture_pair(f"{PLAYER_SPRITE_FILE_PATH}_jump.png")
+        self.fall_texture_pair = arcade.load_texture_pair(f"{PLAYER_SPRITE_FILE_PATH}_fall.png")
         
         self.walk_textures = []
         for i in range(2):
-            texture = arcade.load_texture_pair(f"{SPRITE_FILE_PATH}_walk{i}.png")
+            texture = arcade.load_texture_pair(f"{PLAYER_SPRITE_FILE_PATH}_walk{i}.png")
             self.walk_textures.append(texture)
             
             self.texture = self.idle_texture_pair[0]
@@ -55,19 +56,65 @@ class PlayerSprite(arcade.Sprite):
                 self.cur_texture = 0
             
             self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
-                
-
-class GameWindow(arcade.Window):
+  
+  
+class LaserSprite(arcade.Sprite):
     
-    def __init__(self, width, height, title):
+    def __init__(self):
+        super().__init__()
         
-        super().__init__(width, height, title)
+        self.scale = SPRITE_SCALING_LASER
+  
+class SplashView(arcade.View):
+    
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.DARK_GOLDENROD)
+        
+        arcade.set_viewport(0, SCREEN_WIDTH -1, 0, SCREEN_HEIGHT - 1)    
+                  
+                  
+    def on_draw(self):
+        arcade.start_render()
+        
+        arcade.draw_text(text= "Tanis Roberts - Final Project",
+                         start_x= SCREEN_WIDTH / 2,
+                         start_y= SCREEN_HEIGHT / 2,
+                         color= SPLASH_FONT_COLOUR,
+                         font_size=50,
+                         anchor_x="center")
+        
+        arcade.draw_text(text= "- Applying NEAT AI to a Platformer Game -", 
+                         start_x= SCREEN_WIDTH / 2,
+                         start_y= SCREEN_HEIGHT / 2-30,
+                         color= SPLASH_FONT_COLOUR,
+                         font_size=30, 
+                         anchor_x="center")
+        
+        arcade.draw_text(text= "Click anywhere to continue...", 
+                         start_x= SCREEN_WIDTH / 2,
+                         start_y= SCREEN_HEIGHT / 2-75,
+                         color= SPLASH_FONT_COLOUR,
+                         font_size=20, 
+                         anchor_x="center")
+        
+    def on_mouse_press(self, _x, _y, _button, _modifers):
+        view = GameView()
+        view.setup()
+        self.window.show_view(view)
+
+class GameView(arcade.View):
+    
+    def __init__(self):
+        
+        super().__init__()
         
         self.player_sprite: Optional[PlayerSprite] = None
         
         self.player_list: Optional[arcade.SpriteList] = None
+        self.laser_list: Optional[arcade.SpriteList] = None
         self.wall_list: Optional[arcade.SpriteList] = None
         self.item_list: Optional[arcade.SpriteList] = None
+        self.door_list: Optional[arcade.SpriteList] = None
         self.dynamic_list: Optional[arcade.SpriteList] = None
         self.bg_list: Optional[arcade.SpriteList] = None
         self.hazard_list: Optional[arcade.SpriteList] = None
@@ -82,14 +129,20 @@ class GameWindow(arcade.Window):
         
         self.current_map = DEFAULT_MAP_NUM
         
+        self.score = 0
+        self.death_count = 0
+        
         arcade.set_background_color(BACKGROUND_COLOUR)
         
     def setup(self):
         
         game_map = arcade.tilemap.read_tmx(MAP_FOLDER_PATH + MAP_SWITCHER.get(self.current_map))
         
+        self.laser_list = arcade.tilemap.process_layer(game_map, 'Laser', SPRITE_SCALING_LASER)
+        
         self.wall_list = arcade.tilemap.process_layer(game_map, 'Walls', SPRITE_SCALING_TILES)
         self.item_list = arcade.tilemap.process_layer(game_map, 'Items', SPRITE_SCALING_TILES)
+        self.door_list = arcade.tilemap.process_layer(game_map, 'Door', SPRITE_SCALING_TILES)
         self.dynamic_list = arcade.tilemap.process_layer(game_map, 'Dynamic', SPRITE_SCALING_TILES)
         self.bg_list = arcade.tilemap.process_layer(game_map, 'Background', SPRITE_SCALING_TILES)
         self.hazard_list = arcade.tilemap.process_layer(game_map, 'Hazards', SPRITE_SCALING_TILES)
@@ -114,10 +167,16 @@ class GameWindow(arcade.Window):
                                        friction= PLAYER_FRICTION,
                                        mass= PLAYER_MASS,
                                        moment= arcade.PymunkPhysicsEngine.MOMENT_INF,
-                                       collision_type="player",
+                                       collision_type= "player",
                                        radius= 0.1,
                                        max_horizontal_velocity= PLAYER_MAX_HORIZONTAL_SPEED,
                                        max_vertical_velocity= PLAYER_MAX_VERTICAL_SPEED)
+        
+        # -- Laser List
+        self.physics_engine.add_sprite_list(self.laser_list,
+                                            friction= 0,
+                                            collision_type= "laser",
+                                            body_type= arcade.PymunkPhysicsEngine.STATIC)
         
         # -- Wall List
         self.physics_engine.add_sprite_list(self.wall_list,
@@ -144,6 +203,7 @@ class GameWindow(arcade.Window):
                                             body_type= arcade.PymunkPhysicsEngine.STATIC)
         
         
+        
     
     def on_key_press(self, key, modifiers):
         
@@ -168,7 +228,12 @@ class GameWindow(arcade.Window):
                 print("[current_map: %d]" % self.current_map)
                 print("[Map Path: %s]" % MAP_FOLDER_PATH + MAP_SWITCHER.get(self.current_map))
                 self.setup()
-            
+            elif key == arcade.key.F:
+                self.window.set_fullscreen(not self.window.fullscreen)
+
+                width, height = self.window.get_size()
+                arcade.set_viewport(0, width, 0, height)
+
     
     def on_key_release(self, key, modifers):
         
@@ -228,22 +293,66 @@ class GameWindow(arcade.Window):
         else:
             self.physics_engine.set_friction(self.player_sprite, 1.0)
             
+        for laser in self.laser_list:
+            laser.set_position(laser._get_center_x() + LASER_MOVE_SPEED,laser._get_center_y())
+        
+        item_hit_list = arcade.check_for_collision_with_list(self.player_sprite,self.item_list)
+        
+        for item in item_hit_list:
+            self.score += 1
+            item.remove_from_sprite_lists()
+            
+        laser_hit_list = arcade.check_for_collision_with_list(self.player_sprite,self.laser_list)
+        
+        for beam in laser_hit_list:
+            self.death_count += 1
+            #beam.remove_from_sprite_lists()
+        
         
         self.physics_engine.step()
+        
     
     def on_draw(self):
         arcade.start_render()
         self.item_list.draw()
         self.dynamic_list.draw()
-        self.bg_list.draw()
         self.hazard_list.draw()
+        self.bg_list.draw()
         self.wall_list.draw()
+        self.door_list.draw()
+        self.laser_list.draw()
         self.player_list.draw()
+        
+        score_text = f"Score: {self.score}"
+        deaths_text = f"Deaths: {self.death_count}"
+        
+        left, right, bottom, top = arcade.get_viewport()
+        
+        arcade.draw_text(text= score_text,
+                         start_x= 10 + left,
+                         start_y= top - 40,
+                         color= GAME_FONT_COLOUR,
+                         font_size= 35)
+        
+        arcade.draw_text(text= deaths_text,
+                         start_x= 10 + left,
+                         start_y= top - 70,
+                         color= GAME_FONT_COLOUR,
+                         font_size= 35)
     
     
 def main():
-    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    
+    if OPEN_AT_GAME and IS_DEBUG:
+        # - Go direct to the game, for debug
+        start_view = GameView()
+        start_view.setup()
+    else:
+        # - Show splash screen before starting
+        start_view = SplashView()
+        
+    window.show_view(start_view)
     arcade.run()
     
     
